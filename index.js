e95cc25614e9a3620f0a3b11ea8a72d99411224b
Diego Queiroz dos Santos <admin@dbltecnologia.com.br>
2019-03-15 21:44:14 -0300

    .

--- index.js
+++ index.js
@@ -282,8 +282,8 @@ function createProfileDayweekConfig(req, res){
         let title = 'Perfil Dia semana'
         let id = element.id
 
-        let  start = moment(element.startTime).format("YYYY-MM-DDThh:mm:ss")
-        let  end = moment(element.endTime).format("YYYY-MM-DDThh:mm:ss")
+        let  start = element.startTime
+        let  end = element.endTime
 
         let sql = "INSERT INTO acessos_controle_config (id_profile, datetime_start, datetime_end, title, id_day) \
             VALUES ((SELECT id FROM acessos_controle_perfil ORDER BY id DESC LIMIT 1), '" + start + "', '" + end + "', '" + title + "', " + id + ");";
@@ -337,8 +337,8 @@ function updateProfileDayweekConfig(req, res){
         
         events.forEach(element => {
 
-            let  start = moment(element.startTime).format("YYYY-MM-DDThh:mm:ss")
-            let  end = moment(element.endTime).format("YYYY-MM-DDThh:mm:ss")
+            let  start = element.startTime
+            let  end = element.endTime
 
             let title = 'Perfil Dia semana'
             let idDay = element.id
